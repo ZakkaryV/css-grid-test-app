2536760af2c1c51e5a8b26d56a359b9f9a0bea00
Zakkary Verrilli <zverrilli@ZVirrillis-MacBook-Pro.local>
2018-01-08 23:50:11 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    let navLinks = $('#link-container'),
+        pages    = [$('#about-section'), $("#gallery-section"), $("#shop-section")]
+
+    console.log(navLinks[1])
+});
